e479edbf925dae0b3989027d6a65b48dff3c3038
Aamir Mallick <52884890+Aamir-Mallick@users.noreply.github.com>
2023-03-08 19:17:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { InfiniteScroll } from "./infiniteScroll/InfiniteScroll";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InfiniteScroll />
+    </div>
+  );
+}
---
+++ src/infiniteScroll/InfiniteScroll.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const InfiniteScroll = () => {
+  return <h1>welcome black</h1>;
+};
